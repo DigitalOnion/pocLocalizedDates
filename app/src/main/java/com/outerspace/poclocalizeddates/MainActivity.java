5b261cdaa6ab8b1e0a217141007200c97b18ba5e
lsilva <lsilva@magicleap.com>
2019-04-19 17:11:01 -0400

    Add limit to the year from 1900 to the current

--- app/src/main/java/com/outerspace/poclocalizeddates/MainActivity.java
+++ app/src/main/java/com/outerspace/poclocalizeddates/MainActivity.java
@@ -44,38 +44,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        StringBuilder sb = new StringBuilder();
-
-        String dateStr = sdf.format(new Date());
-        sb.append("Today is ").append(dateStr).append('\n');
-
-        Date date;
-        try {
-            date = sdf.parse(dateStr);
-        } catch (ParseException e) {
-            date = null;
-            sb.append("Error parsing date").append('\n');
-        }
-        if (date != null) {
-            sb.append("day:").append(new SimpleDateFormat("dd", Locale.getDefault()).format(date)).append('\n');
-            sb.append("month:").append(new SimpleDateFormat("MM", Locale.getDefault()).format(date)).append('\n');
-            sb.append("year:").append(new SimpleDateFormat("yyyy", Locale.getDefault()).format(date)).append('\n');
-        }
-
-        DateFormat formatter = DateFormat.getDateInstance(DateFormat.SHORT, Locale.FRANCE);
-        String pattern = ((SimpleDateFormat) formatter).toPattern();
-        String lPattern= ((SimpleDateFormat) formatter).toLocalizedPattern();
-
-        sb.append("Pattern: ").append(pattern).append('\n');
-        sb.append("Local Pattern: ").append(lPattern).append('\n');
-
-        display.setText(sb.toString());
-
-        feedback = findViewById(R.id.feedback);
-        dateInput = findViewById(R.id.date_input);
+        display.setText(exerciseWithDate(new Date()).toString());
 
         init();
-
+        feedback = findViewById(R.id.feedback);
+        dateInput = findViewById(R.id.date_input);
         dateInput.addTextChangedListener(new TextWatcher() {
             @Override
             public void onTextChanged(CharSequence text, int start, int before, int count) {
@@ -104,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void chooseColors(String dateText, char delimiter, char[] mask) {
-        if(dateText.length() == charMask.length && verifyDate(dateText.toString(), delimiter, charMask))
+        if(dateText.length() == mask.length && verifyDate(dateText.toString(), delimiter, mask))
             dateInput.setBackgroundColor(getColor(R.color.colorItsADate));
         else
             dateInput.setBackgroundColor(getColor(R.color.colorNotADate));
@@ -118,7 +91,9 @@ public class MainActivity extends AppCompatActivity {
             return false;
         try {
             Date date = sdf.parse(dateText);
-            return true;
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date);
+            return (cal.get(Calendar.YEAR) > 1900 && date.before(new Date()));
         } catch (ParseException e) {
             return false;
         }
@@ -136,6 +111,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickButtonReset(View view) {
         init();
+    }
+
+    private StringBuilder exerciseWithDate(Date date) {
+        String dateStr = sdf.format(date);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Today is ").append(dateStr).append('\n');
+
+        if (date != null) {
+            sb.append("day:").append(new SimpleDateFormat("dd", Locale.getDefault()).format(date)).append('\n');
+            sb.append("month:").append(new SimpleDateFormat("MM", Locale.getDefault()).format(date)).append('\n');
+            sb.append("year:").append(new SimpleDateFormat("yyyy", Locale.getDefault()).format(date)).append('\n');
+        }
+
+        DateFormat formatter = DateFormat.getDateInstance(DateFormat.SHORT, Locale.FRANCE);
+        String pattern = ((SimpleDateFormat) formatter).toPattern();
+        String lPattern= ((SimpleDateFormat) formatter).toLocalizedPattern();
+
+        sb.append("Pattern: ").append(pattern).append('\n');
+        sb.append("Local Pattern: ").append(lPattern).append('\n');
+        return sb;
     }
 
 }
